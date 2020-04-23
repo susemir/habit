ecd08ee964a6fb1e021773a3737234f3e790d68c
Sujin Han <sujinhan.me@gmail.com>
2020-04-23 13:35:29 +0900

    Data POST 기능 추가
    css 깨지는 부분 오류해결, mongoDB에 입력값(습관명, 컬러코드, 날짜) 정상적으로 들어감

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+import requests
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbhabit
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/record', methods=['POST'])
+def make_record():		# 클라이언트로부터 데이터를 받는 부분
+    name_receive = request.form['name_give']
+    color_receive = request.form['color_give']
+    date_receive = request.form['date_give']
+
+		# mongoDB에 넣는 부분
+    habit = {'name': name_receive, 'color': color_receive, 'date': date_receive }
+
+    db.habits.insert_one(habit)
+
+    return jsonify({'result': 'success'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
