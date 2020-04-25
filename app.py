8d32cd46fc706d3a462527541909a039258644d5
Sujin Han <sujinhan.me@gmail.com>
2020-04-25 14:40:16 +0900

    POST, GET alert
    POST, GET 정상적으로 작동하는 것 확인.

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ def make_record():		# 클라이언트로부터 데이터를 받는 부분
 
     return jsonify({'result': 'success'})
 
+@app.route('/record/', methods = ['GET'])
+def get_record():
+    habit_list = list(db.habits.find({}, {'_id': False}))
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
