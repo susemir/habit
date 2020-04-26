e741d2d0d9665bd50cf59bba540b4aebd15f370d
Sujin Han <sujinhan.me@gmail.com>
2020-04-26 13:19:53 +0900

    data 뿌려주는 게 안됨
    table 만들고 싶다ㅠㅜ

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def make_record():		# 클라이언트로부터 데이터를 받는 부분
 @app.route('/record/', methods = ['GET'])
 def get_record():
     habit_list = list(db.habits.find({}, {'_id': False}))
-    return jsonify({'result': 'success'})
+    return jsonify({'result': 'success'}, {'habit_list': 'habit_list'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
